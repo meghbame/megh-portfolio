adafe1d75ab72dbd923ce48d511fc85e6c260f56
Megh Bame <meghbame@gmail.com>
2026-04-01 00:06:13 +0530

    final fix render port

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ def contact():
 
 if __name__ == '__main__':
     import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
