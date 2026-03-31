0038ad5e080fe8069e1b24b2bc5811571c5c21f6
Megh Bame <meghbame@gmail.com>
2026-03-31 23:59:17 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -15,4 +15,8 @@ def contact():
     return jsonify({"status": "success", "message": "Message received!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
